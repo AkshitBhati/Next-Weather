0a2fa18cb1c08c9b4decfa1a56adf92112c2012a
AkshitBhati <akshitworks24@gmail.com>
2024-06-19 00:02:26 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='border bg-gray-200 p-3 flex items-center'>
+      <h2 className='text-xl font-semibold cursor-pointer'>Weather App</h2>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function app() {
+  return (
+    <div className=''>
+      Weather App
+    </div>
+  )
+}
